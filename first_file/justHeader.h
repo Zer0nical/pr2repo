01b6a36d83a3478648035fd558d6ffe1b119bad2
Zer0Nical <savesallgames@gmail.com>
2024-11-03 12:26:22 +0300

    "First version of this build (main branch)

---
+++ first_file/justHeader.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+auto quadratic = [](int a){
+    cout << pow(a,2);
+};
---
+++ first_file/something.cpp
@@ -0,0 +1,8 @@
+#include "justHeader.h"
+int main()
+{
+    int d;
+    cin >> d;
+    quadratic(d);
+    return 0;
+}
